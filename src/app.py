5b1a4fe7e5334ea0fa1ccc6085175f8417918452
Soumadeep Singha <soumadeep1@gmail.com>
2023-08-10 00:20:44 +0530

    CICD demo

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is a CICD demo using flask app"
+    return "This is a CICD demo to test changes"
 
 if __name__ == "__main__":
     app.run()
