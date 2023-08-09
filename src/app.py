bf82a0e67c3f423c76741ad1a18e9a15e10f6be9
Soumadeep Singha <soumadeep1@gmail.com>
2023-08-10 00:15:55 +0530

    Demo test

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World! I am flask"
+    return "This is a CICD demo using flask app"
 
 if __name__ == "__main__":
     app.run()
